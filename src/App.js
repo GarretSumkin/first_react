e4b9148b59a7bca024473aa42390e6b63e961ff8
garret <alandarenko13@gmail.com>
2022-09-20 23:15:04 +0300

    First level

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import logo from './logo.svg';
+import React from "react";
+// import logo from './logo.svg';
+// import mylogo from './blood_type_coffee.svg';
+import myLogo from './myLogo.png';
 import './App.css';
+import Text from "./components/Text/Text";
 
+const MyFunction = props => {
+    return <div>{props.text}</div>
+};
+const MyFunctionSecond = props => {
+    return <div>{props.href}</div>
+};
 function App() {
   return (
     <div className="App">
+
+      <Text/>
+
+
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+          <p>Hello world</p>
+        <img src={myLogo} className="App-logo" alt="logo" />
+          <MyFunction text="Garret"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+          <MyFunction text="!"/>
+          <MyFunctionSecond href="https://reactjs.org"/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Text/Text.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Text.scss"
+
+const Text = () => {
+    const text = ""
+
+    return (
+        <div className={"text"}>
+            {text}
+        </div>
+    );
+};
+
+export default Text;
